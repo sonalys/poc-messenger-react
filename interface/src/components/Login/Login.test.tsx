c3df3dd27a937c57d6607e49549d8b93c0dd3060
Alysson <alraicon@gmail.com>
2021-08-28 00:01:45 -0300

    test login with enter

--- interface/src/components/Login/Login.test.tsx
+++ interface/src/components/Login/Login.test.tsx
@@ -71,17 +71,29 @@ describe('should set states', () => {
   });
 
   test('should update password when typing', async () => {
-    const setUsernameMock = jest.fn();
+    const setPasswordMock = jest.fn();
 
     jest
       .spyOn(React, 'useState')
       .mockReturnValueOnce(['', jest.fn()])
-      .mockReturnValueOnce(['', setUsernameMock]);
+      .mockReturnValueOnce(['', setPasswordMock]);
 
     const wrapper = mount(<Login getUserName={jest.fn()} />);
     const input = wrapper.find('#login-password');
 
     input.first().simulate('change', { target: { value: '123' } });
-    expect(setUsernameMock).toBeCalledWith('123');
+    expect(setPasswordMock).toBeCalledWith('123');
   });
+});
+
+describe('should login with enter', () => {
+  const performLogin = jest.fn();
+
+  const wrapper = mount(
+    <Login getUserName={jest.fn()} performLogin={performLogin} />
+  );
+  const input = wrapper.find('#login-password');
+
+  input.first().simulate('keyDown', { code: 'Enter' });
+  expect(performLogin).toBeCalled();
 });
